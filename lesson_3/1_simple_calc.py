1955fbdba976b8c117e6f5171daf0fad9d80b8cd
lurtik <danylovildanov2602@gmail.com>
2026-08-22 20:50:38 +0100

    fixed hw

--- lesson_3/1_simple_calc.py
+++ lesson_3/1_simple_calc.py
@@ -3,17 +3,16 @@ operations = ["+", "-", "*", "/"]
 num_1, op, num_2 =  input("Введіть операцію у форматі: a оператор b, де оператор один з + - * /\n"
     "Приклад: 3 + 5\n> ").split()
 num_1, num_2 = int(num_1), int(num_2)
-if num_2 != 0:
-    if op in operations:
-        if op == "+":
-            print(num_1 + num_2)
-        elif op == "-":
+if op in operations:
+    if op == "+":
+        print(num_1 + num_2)
+    elif op == "-":
             print(num_1 - num_2)
-        elif op == "*":
+    elif op == "*":
             print(num_1 * num_2)
-        else:
-            print(num_1 / num_2)
-    else:
-        raise ValueError(f"Немає такої операції {op} у моєму калькуляторі")
+    elif op == "/":
+        if num_2 == 0:
+            raise ZeroDivisionError
+        print(num_1 / num_2)
 else:
-    raise ZeroDivisionError
+    raise ValueError(f"Немає такої операції {op} у моєму калькуляторі")
