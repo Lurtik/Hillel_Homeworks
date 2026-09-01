0c42fd4b9d7c8c01c4ad7d58cd1c8e55315cdc91
lurtik <danylovildanov2602@gmail.com>
2026-09-01 18:48:15 +0100

    fixed hw

--- lesson_5/1_variable_name.py
+++
@@ -1,7 +0,0 @@
-from string import punctuation
-
-sample = input("Enter a sentence: ")
-sample = sample.title().replace(" ", "")
-
-hash_list = [i for i in sample if i not in punctuation]
-print(f"Your hashtag is {"#" + "".join(hash_list)}")
---
+++ lesson_5/3_hashtag.py
@@ -0,0 +1,7 @@
+from string import punctuation
+
+sample = input("Enter a sentence: ")
+sample = sample.title().replace(" ", "")
+
+hash_list = [i for i in sample if i not in punctuation]
+print(f"Your hashtag is {"#" + "".join(hash_list)}")
